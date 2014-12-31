4b7069acaeeeaef1eef5dbac2a87b48c924a4557
Patrick Blaine <pblaine@my.smccd.edu>
2014-12-31 14:36:01 -0800

    added in Facebook SDK

--- ParseTest/AppDelegate.h
+++ ParseTest/AppDelegate.h
@@ -8,10 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : NSObject <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, strong) IBOutlet UIWindow *window;
 
+@property (nonatomic, strong) IBOutlet UIViewController *viewController;
 
 @end
 
